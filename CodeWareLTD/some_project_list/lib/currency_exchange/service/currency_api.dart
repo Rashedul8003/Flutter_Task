cd511a2a29df6832c956adf673882e1e7e3b45fb
Rashed UL <106478932+Rashedul8003@users.noreply.github.com>
2023-07-13 14:34:34 +0600

    okay this app

--- CodeWareLTD/some_project_list/lib/currency_exchange/service/currency_api.dart
+++ CodeWareLTD/some_project_list/lib/currency_exchange/service/currency_api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:some_project_list/currency_exchange/constant/constant.dart';
 import 'package:some_project_list/currency_exchange/model/currency_model.dart';
@@ -27,20 +28,29 @@ class ApiService {
     }
   }
 
-  //  Future<List<CurrencyModel>>getExchange(String baseCountry,String targetCountry)async{
-  //    List<CurrencyModel> currencyModelList = [];
-  //    String url =
-  //        '${base_url}latest?apikey=$apiKey&base_currency=$baseCountry&currencies=$targetCountry';
-  // try{
-  //   final response = await http.get(Uri.parse(url));
-  //   if(response.statusCode==200){
-  //     Map<String, dynamic> json = jsonDecode(response.body);
-  //     Map<String, dynamic> body = jsonDecode(response.body);
-  //
-  //   }
-  // }catch (e){
-  //   return throw e.toString();
-  // }
-  //
-  //  }
+  Future<List<CurrencyModel>> getTerget(
+      String baseCounty, String targetCountry) async {
+    List<CurrencyModel> currencyModelList = [];
+    String url =
+        '${base_url}latest?apikey=$apiKey&base_currency=$baseCounty&currencies=$targetCountry';
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        Map<String, dynamic> json = jsonDecode(response.body);
+        Map<String, dynamic> body = json['data'];
+        body.forEach((key, value) {
+          CurrencyModel currencyModel = CurrencyModel.fromJson(value);
+          currencyModelList.add(currencyModel);
+        });
+        return currencyModelList;
+      } else {
+        return Future.error('somethig worng');
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+    return Future.error("Something went wrong");
+  }
 }
--- CodeWareLTD/some_project_list/lib/currency_exchange/ui/exchange.dart
+++ CodeWareLTD/some_project_list/lib/currency_exchange/ui/exchange.dart
@@ -1,4 +1,7 @@
+import 'package:country_currency_pickers/country.dart';
+import 'package:country_currency_pickers/country_pickers.dart';
 import 'package:flutter/material.dart';
+import 'package:some_project_list/currency_exchange/service/currency_api.dart';
 
 class Exchange extends StatefulWidget {
   const Exchange({Key? key}) : super(key: key);
@@ -8,8 +11,164 @@ class Exchange extends StatefulWidget {
 }
 
 class _ExchangeState extends State<Exchange> {
+  bool isVisible = false;
+  ApiService apiService = ApiService();
+  final _textEditingController = TextEditingController();
+  String selectedBaseCurrency = 'USD';
+  String selectedTargetCurrency = 'BDT';
+  String totalValue = '';
+  Widget _buildDropdownItem(Country country) => Container(
+        child: Row(
+          children: <Widget>[
+            CountryPickerUtils.getDefaultFlagImage(country),
+            SizedBox(
+              width: 8.0,
+            ),
+            Container(
+
+                //color: Colors.redAccent,
+                width: MediaQuery.of(context).size.width / 1.4,
+                // constraints:
+                //     BoxConstraints(maxWidth: MediaQuery.of(context).size.width-60),
+                child: Text(
+                  "${country.currencyName}",
+                  overflow: TextOverflow.ellipsis,
+                )),
+          ],
+        ),
+      );
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Base Currency',
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: CountryPickerDropdown(
+              initialValue: 'us',
+              itemBuilder: _buildDropdownItem,
+              onValuePicked: (Country? country) {
+                selectedBaseCurrency = country?.currencyCode ?? '';
+                print(selectedBaseCurrency);
+                setState(() {});
+              },
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            width: 300,
+            child: TextField(
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(15))),
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Terget Currency',
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: CountryPickerDropdown(
+              initialValue: 'bd',
+              itemBuilder: _buildDropdownItem,
+              onValuePicked: (Country? country) {
+                selectedTargetCurrency = country?.currencyCode ?? '';
+                print(selectedTargetCurrency);
+                setState(() {
+                  isVisible = false;
+                  _textEditingController.clear();
+                });
+              },
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            height: 50,
+            width: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.blue,
+            ),
+            child: TextButton(
+              onPressed: () async {
+                if (_textEditingController.text.isNotEmpty) {
+                  await apiService
+                      .getTerget(selectedBaseCurrency, selectedTargetCurrency)
+                      .then((value) {
+                    if (value != null) {
+                      isVisible = true;
+                      double basValue =
+                          double.parse(_textEditingController.text.toString());
+                      double terValue = double.parse(value[0].value.toString());
+                      double total = basValue * terValue;
+                      totalValue = total.toStringAsFixed(2).toString();
+                      print(totalValue);
+                      setState(() {});
+                    }
+                  });
+                }
+                print('TextButton');
+              },
+              child: Text('Exchange',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  )),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          if (isVisible)
+            Center(
+              child: Text(
+                '$selectedTargetCurrency : $totalValue',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 25),
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
